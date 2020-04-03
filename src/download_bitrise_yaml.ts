7af5d8bb4c242ff2dc2d89fdac9f92c9ab52a937
IKEDA Sho <suicaicoca@gmail.com>
2020-04-03 14:20:36 +0900

    Add basic implementation

---
+++ src/download_bitrise_yaml.ts
@@ -0,0 +1,19 @@
+import * as core from '@actions/core'
+import * as http from '@actions/http-client'
+import * as fs from 'fs'
+
+export async function downloadBitriseYaml(
+  appSlug: string,
+  outputPath: string,
+  bitriseToken: string
+): Promise<void> {
+  const client = new http.HttpClient('download-bitrise-yaml')
+  const res = await client.get(
+    `https://api.bitrise.io/v0.1/apps/${appSlug}/bitrise.yml`,
+    {
+      Authorization: bitriseToken
+    }
+  )
+  const body = await res.readBody()
+  await fs.promises.writeFile(outputPath, body)
+}
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,17 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import {downloadBitriseYaml} from './download_bitrise_yaml'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    const appSlug = core.getInput('appSlug', {required: true})
+    core.debug(`appSlug: ${appSlug}`)
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const outputPath = core.getInput('outputPath')
 
-    core.setOutput('time', new Date().toTimeString())
+    const bitriseToken = core.getInput('bitriseToken', {required: true})
+    core.setSecret(bitriseToken)
+
+    await downloadBitriseYaml(appSlug, outputPath, bitriseToken)
   } catch (error) {
     core.setFailed(error.message)
   }
